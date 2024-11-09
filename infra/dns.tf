9e56dec67797569a1919d18ea4592ae97c2e4716
Jakob Lund Johannessen <jakob-lj@hotmail.com>
2024-11-09 15:44:14 +0100

    fix: remove usage of hard coded domain

--- infra/dns.tf
+++ infra/dns.tf
@@ -10,7 +10,7 @@ output "zoneid" {
 resource "aws_route53_record" "node" {
   for_each = aws_instance.nodes
   provider = aws.dns
-  name     = "node${each.key}.jlj.kurs.capragruppen.tech"
+  name     = "node${each.key}.${local.base_domain}"
   type     = "CNAME"
   ttl      = 60
   zone_id  = data.aws_route53_zone.delegated_zone.id
